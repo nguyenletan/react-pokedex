8aa8d1e6301d198dbfefdfcda73d8a521287a9a7
nguyenlet <nguyenletan@gmail.com>
2018-04-16 15:54:34 +0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import PokeList from './PokeList';
+import DetailView from './DetailView';
+import Pokemon from '../Pokemon'
+import './styles/App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemon: {}
+    };
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick(id) {
+    console.log(id);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(res => res.json())
+    .then(data => {
+      const pokemon = new Pokemon(data);
+      this.setState({pokemon});
+      console.log(pokemon);
+    })
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PokeList handleOnClick={this.handleOnClick}/>
+        <DetailView pokemon={this.state.pokemon}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/PokeCell.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import sprites from '../assets/sprites.png';
+import './styles/PokeCell.css';
+
+const PokeCell = ({pokeClass, handleOnClick }) => {
+  const { id, backgroundPosition } = pokeClass;
+  const style = { backgroundImage: `url(${sprites})`, backgroundPosition  };
+
+  return <button style={style} className="poke-cell" onClick={() => handleOnClick(id)}>
+
+  </button>;
+};
+
+export default PokeCell;
